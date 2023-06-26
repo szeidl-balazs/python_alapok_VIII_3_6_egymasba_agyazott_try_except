79cf6afbc91d5c1aa799927e54a785ce4dada5a9
bszeidl <szeidl.balazs@hotmail.com>
2023-06-26 13:20:41 +0200

    Lesson completed, code runs well.

--- filereading.py
+++ filereading.py
@@ -1,19 +1,28 @@
-#file name opener function to open the file and print the content:
+#file name opener function to open the file and return the content:
 def read_file(file_name):
     try:
-        file_name
         target_file = open(file_name)
         content = target_file.read()
-        target_file.close()
-        print(content)
 
     except OSError:
         print('Hiba a fájl kezelése közben!')
 
+    else:
+        return content               
 
-#file nam asker function to ask for file name and return it:
+    finally:
+        target_file.close()
+
+
+
+#file name asker function to ask for file name and return it:
 def get_file_name():
     return input('Which file you want to open? ')
 
-#call the file opener function with the file name asker as argument:
-read_file(get_file_name())
+#printer function that calls the opener function which returns the content:
+def print_content(file_name):
+    
+    print(read_file(file_name))
+
+#call the name asker function that returns the name:
+print_content(get_file_name())
